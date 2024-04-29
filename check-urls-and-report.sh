9fbfe98bc2383a874b5a6cc6fc6502c53cc03fa8
Steven Van Acker <steven@singularity.be>
2024-04-29 06:37:46 +0200

    leave a timestamp on logfile to track when cronjob ran

--- check-urls-and-report.sh
+++ check-urls-and-report.sh
@@ -4,6 +4,9 @@ HERE=$(dirname $(readlink -f $0))
 checkscript=$(readlink -f $HERE/check-urls.py)
 logfile=$(readlink -f $HERE/logfile.txt)
 
+# Leave a timestamp
+touch $logfile
+
 (
 $checkscript | sendteams.py -t "URL Checker" 2>&1 
 ) | tee -a $logfile
